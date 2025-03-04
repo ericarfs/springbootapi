bdb01d4ac4a4aaffddd5ba3dba03818b0d591d2d
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-04 20:35:55 -0300

    Adding user model and controller

---
+++ src/main/java/com/ericarfs/demo/controllers/UserController.java
@@ -0,0 +1,19 @@
+package com.ericarfs.demo.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ericarfs.demo.entities.User;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+	
+	@GetMapping
+	public ResponseEntity<User> listUsers(){
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+}
