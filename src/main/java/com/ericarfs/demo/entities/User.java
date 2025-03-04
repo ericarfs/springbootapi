39d7e5146549224658edc37c2857c011b2747d75
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-04 20:48:40 -0300

    Adding user model and controller

--- src/main/java/com/ericarfs/demo/entities/User.java
+++ src/main/java/com/ericarfs/demo/entities/User.java
@@ -3,6 +3,7 @@ package com.ericarfs.demo.entities;
 import java.io.Serializable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -18,9 +19,12 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
+@Entity
 @Table(name="usuarios")
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
