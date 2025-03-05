bde947d8fc6eab90ea57ddaf4a96022802b030e6
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-05 13:24:19 -0300

    Adding order status enum

--- src/main/java/com/ericarfs/demo/entities/Order.java
+++ src/main/java/com/ericarfs/demo/entities/Order.java
@@ -3,6 +3,7 @@ package com.ericarfs.demo.entities;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.ericarfs.demo.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
@@ -23,6 +24,7 @@ public class Order implements Serializable{
 	
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone="GMT")
 	private Instant moment;
+	private Integer status;
 	@ManyToOne
 	@JoinColumn(name="client_id")
 	private User client;
@@ -31,10 +33,11 @@ public class Order implements Serializable{
 		super();
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus status, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setStatus(status);
 		this.client = client;
 	}
 
@@ -52,6 +55,15 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return OrderStatus.valueOf(status);
+	}
+
+	public void setStatus(OrderStatus status) {
+		if(status != null)
+			this.status = status.getCode();
 	}
 
 	public User getClient() {
