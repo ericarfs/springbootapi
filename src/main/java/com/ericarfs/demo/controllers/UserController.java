9285a68129aff5e7d7d66d8da0b5f4f3cb1b7411
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-11 16:51:10 -0300

    Adding Insert, Update and Delete User methods

--- src/main/java/com/ericarfs/demo/controllers/UserController.java
+++ src/main/java/com/ericarfs/demo/controllers/UserController.java
@@ -1,13 +1,19 @@
 package com.ericarfs.demo.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ericarfs.demo.entities.User;
 import com.ericarfs.demo.services.UserService;
@@ -30,4 +36,24 @@ public class UserController {
 		User u = userService.findById(id);
 		return ResponseEntity.ok().body(u);
 	}
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj){
+		User u = userService.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(u);
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		User u = userService.update(id, obj);
+		return ResponseEntity.ok().body(u);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
 }
--- src/main/java/com/ericarfs/demo/services/UserService.java
+++ src/main/java/com/ericarfs/demo/services/UserService.java
@@ -29,6 +29,26 @@ public class UserService{
 		Optional<User> obj = userRepository.findById(id);
 		return obj.get();
 	}
+	
+	public User insert(User obj) {
+		return userRepository.save(obj);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = userRepository.getReferenceById(id);
+		updateData(entity,obj);
+		return userRepository.save(entity);
+	}
+	
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
+
+	public void delete(Long id) {
+		userRepository.deleteById(id);
+	}
 
 
 }
