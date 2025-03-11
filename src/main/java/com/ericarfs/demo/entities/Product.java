9e31fc2bbdb61ffe44b678f6bc2a3437c1d12d80
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-11 15:45:11 -0300

    Adding Product-OrderItem Association

--- src/main/java/com/ericarfs/demo/entities/Product.java
+++ src/main/java/com/ericarfs/demo/entities/Product.java
@@ -2,8 +2,12 @@ package com.ericarfs.demo.entities;
 
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -31,6 +36,9 @@ public class Product implements Serializable{
 	@ManyToMany()
 	@JoinTable(name="tb_product_category", joinColumns = @JoinColumn(name="product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private List<Category> categories = new ArrayList<>();
+	
+	@OneToMany(mappedBy = "id.product")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Product() {
 		super();
@@ -107,8 +115,18 @@ public class Product implements Serializable{
         		categories.add(category);
         }
     }
-
-
+    
+    @JsonIgnore
+    public Set<Order> getOrders(){
+    	Set<Order> set = new HashSet<>();
+    	
+    	for (OrderItem x : items) {
+    		set.add(x.getOrder());
+    	}
+    	
+    	return set;
+    }
+    
 	@Override
 	public int hashCode() {
 		return Objects.hash(categories, description, id, imgURL, name, price);
