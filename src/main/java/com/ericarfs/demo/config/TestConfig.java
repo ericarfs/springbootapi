691a3729203040da20bb673c550843940fdd854a
Erica Ribeiro <ericaribeiro@usp.br>
2025-03-11 15:59:00 -0300

    Adding Payment Association

--- src/main/java/com/ericarfs/demo/config/TestConfig.java
+++ src/main/java/com/ericarfs/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.ericarfs.demo.entities.Category;
 import com.ericarfs.demo.entities.Order;
 import com.ericarfs.demo.entities.OrderItem;
+import com.ericarfs.demo.entities.Payment;
 import com.ericarfs.demo.entities.Product;
 import com.ericarfs.demo.entities.User;
 import com.ericarfs.demo.entities.enums.OrderStatus;
@@ -38,6 +39,7 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private OrderItemRepository orderItemRepository;
+	
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -78,6 +80,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2,oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
